147b8a81e5607e1da358183a4ba948a2db0553e1
Agasi Miroyan <limekick@mail.ru>
2024-04-25 19:45:22 +0400

    most commented done

--- apps/blog/templatetags/blog_tags.py
+++ apps/blog/templatetags/blog_tags.py
@@ -1,6 +1,7 @@
+from django.db.models import Count
 from django.template import Library
 
-from ..models import Post
+from ..models import Post, Comment
 
 register = Library()
 
@@ -8,3 +9,9 @@ register = Library()
 @register.inclusion_tag('blog/most_popular.html')
 def most_popular():
     return {'posts':Post.custom.order_by('-views')[:5]}
+
+
+@register.inclusion_tag('blog/most_commented.html')
+def most_commented():
+    posts = Post.custom.annotate(total=Count('comments')).filter(total__gte=1).order_by('-total')
+    return {'posts': posts}
