c9bb4e465ad7960fd13076b5c8b61defea3ffaba
Agasi Miroyan <limekick@mail.ru>
2024-04-26 15:43:22 +0400

    sommernote

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -42,7 +42,7 @@ class Post(models.Model):
     title = models.CharField(verbose_name="Название записи", max_length=255)
     slug = models.SlugField(verbose_name="URL", max_length=255, blank=True)
     description = models.TextField(verbose_name="Краткое описание", max_length=500)
-    text = RichTextField(config_name='awesome_ckeditor', verbose_name="Полный текст записи")
+    text = models.TextField(verbose_name="Полный текст записи")
     category = TreeForeignKey(
         "Category",
         on_delete=models.PROTECT,
